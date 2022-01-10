6b6e9040a4dd779f036bfe611005b7061bf57252
Shlok D. Mange <shlok_mange@protonmail.com>
2022-01-10 12:57:07 +0400

    wallet balance function

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const {
     Connection,
     PublicKey,
     clusterApiUrl,
-    Keypair
+    Keypair,
+    LAMPORTS_PER_SOL
 } = require('@solana/web3.js')
 
 // generate new wallet
 const newPair = new Keypair()
+const publicKey = new PublicKey(newPair.publicKey).toString()
+const secretKey = newPair.secretKey
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(secretKey)
+        const walletBalance = await connection.getBalance(myWallet.publicKey)
+        console.log(`Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+    } catch(err) {
+        console.error(err)
+    }
+}
