d4276121c43520e89e6416ecb3de6185b48d229a
Shlok D. Mange <shlok_mange@protonmail.com>
2022-01-10 12:59:10 +0400

    airdrop function

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ const getWalletBalance = async () => {
         console.error(err)
     }
 }
+
+const airdropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.error(err)
+    }
+}
